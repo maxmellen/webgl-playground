b8c8c28deb536d8a2b0b1ad534795bf2133366ea
Maximilien Mellen <maxmellen0@gmail.com>
2020-01-31 15:03:09 +0100

    Revert to spinning triangle

--- main.js
+++ main.js
@@ -6,102 +6,69 @@ if (!canvas) throw new Error("Could not get canvas.");
 let gl = canvas.getContext("webgl");
 if (!gl) throw new Error("Could not get WebGL context.");
 
-const VERT_GLSL = `
-  attribute vec4 a_position;
-  attribute vec2 a_texcoord;
-  uniform float u_angle;
-  varying vec2 v_texcoord;
+let vertGlsl = `
+attribute vec2 a_position;
+uniform float u_angle;
 
-  mat2 rotate2D(float a) {
-    return mat2(
-      cos(a), -sin(a),
-      sin(a), cos(a)
-    );
-  }
+mat2 rotate(float a) {
+  return mat2(
+     cos(a), sin(a),
+    -sin(a), cos(a)
+  );
+}
 
-  void main() {
-    gl_Position = a_position;
-    gl_Position.xy *= rotate2D(u_angle);
-    gl_Position.xy /= 3.5;
-    gl_Position.y *= -1.0;
+void main() {
+  gl_Position = vec4(a_position * rotate(u_angle), 0.0, 1.0);
+}
+`;
 
-    v_texcoord = a_texcoord;
-  }
-`
+let fragGlsl = `
+precision mediump float;
 
-const FRAG_GLSL = `
-  precision mediump float;
-
-  varying vec2 v_texcoord;
-  uniform sampler2D u_texture;
-
-  void main() {
-    gl_FragColor = texture2D(u_texture, v_texcoord);
-  }
-`
-
-let vertexShader = createShader(gl, gl.VERTEX_SHADER, VERT_GLSL);
-let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, FRAG_GLSL);
-let program = createProgram(gl, vertexShader, fragmentShader);
-gl.useProgram(program);
+void main() {
+  gl_FragColor = vec4(1.0, 0.0, 0.5, 1.0);
+}
+`;
 
 let positions = Float32Array.of(
-  0, 0, 2.5, 0, 0, 1,
-  0, 1, 2.5, 0, 2.5, 1,
-  0, 1, 1, 1, 0, 3.5,
-  0, 3.5, 1, 1, 1, 3.5,
-  1, 1.5, 2, 1.5, 1, 2.25,
-  1, 2.25, 2, 1.5, 2, 2.25
+  0, 0,
+  0, 0.5,
+  1, 0
 );
 
+let degAngle = 0;
 
-let texCoords = Float32Array.of(
-  38, 44, 218, 44, 38, 85,
-  38, 85, 218, 44, 218, 85,
-  38, 85, 113, 85, 38, 223,
-  38, 223, 113, 85, 113, 223,
-  113, 112, 203, 112, 113, 151,
-  113, 151, 203, 112, 203, 151
-).map(x => x / 255);
+let vertShader = compileShader(gl, gl.VERTEX_SHADER, vertGlsl);
+let fragShader = compileShader(gl, gl.FRAGMENT_SHADER, fragGlsl);
 
-let degA = 0;
+let program = gl.createProgram();
+gl.attachShader(program, vertShader);
+gl.attachShader(program, fragShader);
+gl.linkProgram(program);
 
-let positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+  let info = gl.getProgramInfoLog(program);
+  gl.deleteProgram(program);
+  throw new Error(`Could not link program: ${info}`);
+}
+
+gl.useProgram(program);
+
+let vertexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 
-let texCoordBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
+let positionAttribLocation = gl.getAttribLocation(program, "a_position");
+gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(positionAttribLocation);
 
-let texture = gl.createTexture();
-gl.bindTexture(gl.TEXTURE_2D, texture);
-gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, Uint8Array.of(0, 0, 255, 255));
+let angleUniformLocation = gl.getUniformLocation(program, "u_angle");
 
-let image = new Image();
-image.src = "f-texture.png";
-image.addEventListener("load", () => {
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-  gl.generateMipmap(gl.TEXTURE_2D);
-});
-
-let aPositionLocation = gl.getAttribLocation(program, "a_position");
-gl.enableVertexAttribArray(aPositionLocation);
-
-let aTexCoordLocation = gl.getAttribLocation(program, "a_texcoord");
-gl.enableVertexAttribArray(aTexCoordLocation);
-
-let uAngleLocation = gl.getUniformLocation(program, "u_angle");
-let uTextureLocation = gl.getUniformLocation(program, "u_texture");
-gl.uniform1i(uTextureLocation, 0);
-
-gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 gl.clearColor(0, 0, 0, 0);
 
 requestAnimationFrame(drawScene);
 
-function createShader(gl, type, source) {
+function compileShader(gl, type, source) {
   let shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
@@ -109,39 +76,17 @@ function createShader(gl, type, source) {
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     let info = gl.getShaderInfoLog(shader);
     gl.deleteShader(shader);
-    throw new Error(`Could not compile WebGL shader: ${info}`);
+    throw new Error(`Could not compile shader: ${info}`);
   }
 
   return shader;
 }
 
-function createProgram(gl, vertexShader, fragmentShader) {
-  let program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    let info = gl.getProgramInfoLog(program);
-    gl.deleteProgram(program);
-    throw new Error(`Could not compile WebGL program: ${info}`);
-  }
-
-  return program;
-}
-
 function drawScene() {
-  gl.uniform1f(uAngleLocation, degA++ * Math.PI / 180)
+  gl.uniform1f(angleUniformLocation, degAngle++ / 180 * Math.PI);
 
   gl.clear(gl.COLOR_BUFFER_BIT);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.vertexAttribPointer(aPositionLocation, 2, gl.FLOAT, false, 0, 0);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-  gl.vertexAttribPointer(aTexCoordLocation, 2, gl.FLOAT, false, 0, 0);
-
-  gl.drawArrays(gl.TRIANGLES, 0, 18);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 
   requestAnimationFrame(drawScene);
 }
