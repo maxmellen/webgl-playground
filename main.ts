4b0aafa6e7d606489101abdc54fb531bc28e6373
Maximilien Mellen <maxmellen0@gmail.com>
2020-05-12 22:29:36 +0200

    Draw Fire Storm Cube shader
    Original: https://www.shadertoy.com/view/ldyyWm

--- main.ts
+++ main.ts
@@ -28,20 +28,54 @@ uniform vec2 u_resolution;
 uniform float u_time;
 out vec4 out_color;
 
-void mainImage(out vec4 e) {
-  vec3 d = 0.5 - vec3(gl_FragCoord.xy, 1) / u_resolution.y, p, o;
-  for (int i = 0; i < 32; i++) {
-    o = p;
-    o.z -= u_time * 9.0;
-    float a = o.z * 0.1;
-    o.xy *= mat2(cos(a), sin(a), -sin(a), cos(a));
-    p += (0.1- length(cos(o.xy) + sin(o.yz))) *d;
-  }
-  e = vec4((sin(p) + vec3(2.0, 5.0, 9.0)) / length(p) * vec3(1.0), 1.0);
+#define iResolution u_resolution
+#define iTime u_time
+
+float burn;
+
+mat2 rot(float a)
+{
+  float s=sin(a), c=cos(a);
+  return mat2(s, c, -c, s);
+}
+
+float map(vec3 p)
+{
+  float d = max(max(abs(p.x), abs(p.y)), abs(p.z)) - .5;
+  burn = d;
+
+  mat2 rm = rot(-iTime/3. + length(p));
+  p.xy *= rm, p.zy *= rm;
+
+  vec3 q = abs(p) - iTime;
+  q = abs(q - round(q));
+
+  rm = rot(iTime);
+  q.xy *= rm, q.xz *= rm;
+
+  d = min(d, min(min(length(q.xy), length(q.yz)), length(q.xz)) + .01);
+
+  burn = pow(d - burn, 2.);
+
+  return d;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+  vec3 rd = normalize(vec3(2.*fragCoord-iResolution.xy, iResolution.y)),
+  ro = vec2(0, -2).xxy;
+
+  mat2 r1 = rot(iTime/4.), r2 = rot(iTime/2.);
+  rd.xz *= r1, ro.xz *= r1, rd.yz *= r2, ro.yz *= r2;
+
+  float t = .0, i = 24. * (1. - exp(-.2*iTime-.1));
+  for(;i-->0.;)t += map(ro+rd*t) / 2.;
+
+  fragColor = vec4(1.-burn, exp(-t), exp(-t/2.), 1);
 }
 
 void main() {
-  mainImage(out_color);
+  mainImage(out_color, gl_FragCoord.xy);
 }
 `;
 
