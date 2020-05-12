e0b8a09f1b35cf85f8718bebef0714792c3f0001
Maximilien Mellen <maxmellen0@gmail.com>
2020-05-12 22:54:24 +0200

    Render Glowing petals shader with WebGL 1.0
    Original: https://www.shadertoy.com/view/Xs3yRM

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 let canvas = document.querySelector("canvas")!;
-let gl = canvas.getContext("webgl2");
+let gl = canvas.getContext("webgl");
 
 if (!gl) {
   throw new Error("This browser does not support WebGL 2.");
 }
 
-let scaleFactor = 4;
+let scaleFactor = 2;
 
 let positions = Float32Array.of(
   ...[...[-1, 1], ...[-1, -1], ...[1, 1]],
@@ -13,8 +13,8 @@ let positions = Float32Array.of(
 );
 
 // language=GLSL
-let vsSource = `#version 300 es
-in vec2 a_position;
+let vsSource = `#version 100
+attribute vec2 a_position;
 
 void main() {
   gl_Position = vec4(a_position, 0.0, 1.0);
@@ -22,60 +22,111 @@ void main() {
 `;
 
 // language=GLSL
-let fsSource = `#version 300 es
-precision mediump float;
+let fsSource = `#version 100
+precision highp float;
 uniform vec2 u_resolution;
 uniform float u_time;
-out vec4 out_color;
 
 #define iResolution u_resolution
 #define iTime u_time
 
-float burn;
+// variant of https://www.shadertoy.com/view/Mstczr
 
-mat2 rot(float a)
-{
-  float s=sin(a), c=cos(a);
-  return mat2(s, c, -c, s);
+vec3 glow = vec3(0.);
+float glow_intensity = .01;
+vec3 glow_color = vec3(.5, .8, .5);
+
+float smin(float a, float b) {
+  float k = 3.;
+  float res = exp(-k*a) + exp(-k*b);
+  return -log(res) / k;
 }
 
-float map(vec3 p)
-{
-  float d = max(max(abs(p.x), abs(p.y)), abs(p.z)) - .5;
-  burn = d;
+mat2 r2d(float a) {
+  float c = cos(a), s = sin(a);
+  return mat2(c, s, -s, c);
+}
 
-  mat2 rm = rot(-iTime/3. + length(p));
-  p.xy *= rm, p.zy *= rm;
+vec2 amod(vec2 p, float m) {
+  float a = mod(atan(p.x, p.y) -m*.5, m) - m * .5;
+  return vec2(cos(a), sin(a)) * length(p);
+}
 
-  vec3 q = abs(p) - iTime;
-  q = abs(q - round(q));
+float de(vec3 p) {
 
-  rm = rot(iTime);
-  q.xy *= rm, q.xz *= rm;
+  p.xy *= r2d(iTime*.1 + p.z);
+  p.xz *= r2d(3.14/2.);
 
-  d = min(d, min(min(length(q.xy), length(q.yz)), length(q.xz)) + .01);
 
-  burn = pow(d - burn, 2.);
 
+  p.zy = amod(p.zy, .785);
+
+  p.y = abs(p.y) - .4;
+  p.z = abs(p.z) - .4;
+  if (p.z > p.y) p.yz = p.zy;
+
+
+  vec3 q = p;
+
+  p.xy *= r2d(-3.14 / 3.);
+  p.xz *= r2d(iTime);
+  p.x += cos(p.y*8.)*.2;
+  p.z += sin(p.y*4.)*.2;
+  float d = (length(p.xz) - .1);
+
+  p = q;
+  p.xy *= r2d(3.14 / 3.);
+  p.xz *= r2d(iTime);
+  p.x += cos(p.y*8.)*.2;
+  p.z += sin(p.y*4.)*.2;
+
+  d = smin(d, (length(p.xz) - .1));
+
+  p = q;
+  p.xz *= r2d(iTime);
+  p.x += cos(p.y*8.)*.2;
+  p.z += sin(p.y*4.)*.2;
+  
+  d = smin(d, (length(p.xz) - .1));
+  
+  p = q;
+  p.xy *= r2d(3.14 / 2.);
+  p.xz *= r2d(iTime);
+  p.x += cos(p.y*8.)*.2;
+  p.z += sin(p.y*4.)*.2;
+  
+  d = smin(d, (length(p.xz) - .1));
+  
+  // trick extracted from balkhan https://www.shadertoy.com/view/4t2yW1
+  glow += glow_color * .025 / (.01 + d*d);
   return d;
 }
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
-  vec3 rd = normalize(vec3(2.*fragCoord-iResolution.xy, iResolution.y)),
-  ro = vec2(0, -2).xxy;
+  vec2 uv = ( fragCoord - .5*iResolution.xy ) / iResolution.y;
 
-  mat2 r1 = rot(iTime/4.), r2 = rot(iTime/2.);
-  rd.xz *= r1, ro.xz *= r1, rd.yz *= r2, ro.yz *= r2;
+  vec3 ro = vec3(0., 0, 6. + cos(iTime)), p;
+  vec3 rd = normalize(vec3(uv, -1));
+  p = ro;
 
-  float t = .0, i = 24. * (1. - exp(-.2*iTime-.1));
-  for(;i-->0.;)t += map(ro+rd*t) / 2.;
+  float t = 0.;
+  for (float i = 0.; i < 1.; i += .01) {
+    p = ro + rd * t;
+    float d = de(p);
+    if (d < .001 || t > 8.) break;
+    t += d * .2; // avoid clipping, enhance the glow
+  }
 
-  fragColor = vec4(1.-burn, exp(-t), exp(-t/2.), 1);
+  vec3 c = vec3(.9, .05 + cos(iTime)*.1, .2);
+  c.r *= p.y + p.z;
+  c += glow * glow_intensity;
+
+  fragColor = vec4(c, 1.);
 }
 
 void main() {
-  mainImage(out_color, gl_FragCoord.xy);
+  mainImage(gl_FragColor, gl_FragCoord.xy);
 }
 `;
 
@@ -114,14 +165,14 @@ resize(gl);
 
 let startTime = Date.now();
 
-function resize(gl: WebGL2RenderingContext) {
+function resize(gl: WebGLRenderingContext) {
   gl.canvas.width = canvas.clientWidth / scaleFactor;
   gl.canvas.height = canvas.clientHeight / scaleFactor;
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   gl.uniform2f(resolutionUniform, gl.canvas.width, gl.canvas.height);
 }
 
-function draw(gl: WebGL2RenderingContext) {
+function draw(gl: WebGLRenderingContext) {
   gl.clearColor(0, 0, 0, 0);
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.uniform1f(timeUniform, (Date.now() - startTime) / 1000);
@@ -129,7 +180,7 @@ function draw(gl: WebGL2RenderingContext) {
 }
 
 function compileShader(
-  gl: WebGL2RenderingContext,
+  gl: WebGLRenderingContext,
   type: GLenum,
   source: string
 ): WebGLShader {
@@ -148,7 +199,7 @@ function compileShader(
 }
 
 function linkProgram(
-  gl: WebGL2RenderingContext,
+  gl: WebGLRenderingContext,
   vs: WebGLShader,
   fs: WebGLShader
 ): WebGLProgram {
