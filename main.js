768bc9f0539baaf3dc0074cd0a78d204fa97a811
Maximilien Mellen <maxmellen0@gmail.com>
2020-05-13 00:25:25 +0200

    Draw grid_0 shader by balkhan
    Original: https://www.shadertoy.com/view/MdVBWz

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// Original shader by lsdlive on Shadertoy
-// https://www.shadertoy.com/view/MdGczK
+// Original shader by balkhan on Shadertoy
+// https://www.shadertoy.com/view/MdVBWz
 let canvas = document.querySelector("canvas");
 let gl = canvas.getContext("webgl");
 if (!gl) {
     throw new Error("This browser does not support WebGL 2.");
 }
-let scaleFactor = 2;
+let scaleFactor = 1;
 let positions = Float32Array.of(...[...[-1, 1], ...[-1, -1], ...[1, 1]], ...[...[1, 1], ...[-1, -1], ...[1, -1]]);
 // language=GLSL
 let vsSource = `#version 100
@@ -24,76 +24,179 @@ uniform float u_time;
 #define iResolution u_resolution
 #define iTime u_time
 
-// @lsdlive
+/*
+* License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+*/
 
+float 	t;
 
-float sc(vec3 p, float d){p=abs(p);p=max(p,p.yzx);return min(p.x,min(p.y,p.z))-d;}
-mat2 r2d(float a){float c=cos(a),s=sin(a);return mat2(c,s,-s,c);}
-float re(float p,float d){return mod(p-d*.5,d)-d*.5;}
-void amod(inout vec2 p, float m){float a=re(atan(p.x,p.y),m);p=vec2(cos(a),sin(a))*length(p);}
-void mo(inout vec2 p, vec2 d){p.x=abs(p.x)-d.x;p.y=abs(p.y)-d.y;if(p.y>p.x)p=p.yx;}
+#define I_MAX		100
+#define E			0.0001
+#define FAR			30.
 
+#define UNIFORM_ROTATION
 
-float scc(vec3 p, float d){
-  float c1 = length(p.xy) - d;
-  float c2 = length(p.xz) - d;
-  float c3 = length(p.zy) - d;
-  return min(c1,min(c2,c3));
-}
+vec4	march(vec3 pos, vec3 dir);
+vec3	camera(vec2 uv);
+vec3	calcNormal(in vec3 pos, float e, vec3 dir);
+vec2	rot(vec2 p, vec2 ang);
+void	rotate(inout vec2 v, float angle);
+float	mylength(vec2 p);
+float	mylength(vec3 p);
 
-float g=0.;
-float de(vec3 p){
-  //p.y-=1.;
-  p.xy*=r2d(iTime*.3);
+vec3	id;
+vec3	h;
 
-  p.xy*=r2d(p.z*.3);
-
-  p.z=re(p.z,9.);
-
-  amod(p.xy, 6.28/4.);
-  mo(p.xy, vec2(2., .3));
-  amod(p.xy, 6.28/8.3);
-  mo(p.zy, vec2(1., .3));
-
-  p.x=abs(p.x)-3.;
-
-  p.y=abs(p.y)-2.;
-  p.xy*=r2d(.5);
-
-  float d = sc (p,.5);
-
-  p.xy*=r2d(iTime*.3);
-  d = min(d, -scc(p,1.));
-  g+=.01/(.01+d*d);
-  return d;
-}
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+void mainImage(out vec4 c_out, in vec2 f)
 {
-  vec2 uv = fragCoord/iResolution.xy -.5;
-  uv.x*=iResolution.x/iResolution.y;
+  h *= 0.;
+  t = iTime;
+  vec3	col = vec3(0., 0., 0.);
+  vec2	R = iResolution.xy, uv  = (f-R*.5)/R.yy;
+  vec3	dir = camera(uv);
+  vec3	pos = vec3(-.0, .0, 2.-iTime);
 
-  vec3 ro=vec3(0,0,-3.+iTime);
-  vec3 rd=normalize(vec3(uv,1));
+  vec4	inter = (march(pos, dir));
 
-  vec3 p;
-  float t=0.;
-  float ri;
-  for(float i=0.;i<1.;i+=.01){
-    ri=i;
-    p=ro+rd*t;
-    float d=de(p);
-    //if(d<.001)break;
-    d=max(abs(d), .005);
-    t+=d*.3;
+  col = h*1.-.5;
+
+  c_out =  vec4(col, h.x);
+}
+
+float	scene(vec3 p)
+{
+  float	mind = 1e5;
+  p.z -= iTime*-1.725;
+
+  p.y += sin(iTime*-1.+p.z*.5)*.5;
+  p.x += cos(iTime*-1.+p.z*.5)*.5;
+  rotate(p.xy, p.z*.25 + 1.0*sin(p.z*.125 - iTime*0.5) + 0.*iTime);
+
+  float	tube = max(-(length(p.yx)-2.), (length(p.yx)-8.));
+  tube = max(tube, p.z-10.-0./length(p.yx*.06125) );
+  tube = max(tube, -p.z-10.-0./length(p.yx*.06125) );
+  vec3	pr = p;
+  pr.xy = (fract(p.xy*.5)-.5)*1.;
+  id = vec3(floor(p.xy*1.-.0), (floor(p.z*.5)-.0)*4.);
+
+  pr.xy = abs(pr.xy)-.2505;
+  #ifdef UNIFORM_ROTATION
+  rotate(pr.xy, iTime);
+  #else
+  rotate(pr.xy, clamp( + (mod(id.x*.5, 2.)-1. <= 0. ? 1. : -1.) + (mod(id.y*.5, 2.)-1. <= 0. ? 1. : -1.), -2., 2.) * iTime*.5 );
+  #endif
+  pr.z = (fract(pr.z*.5)-.5)*4.;
+  mind = mylength(vec2(mylength(pr.xyz)-.31, pr.z ))-.084;
+  return(mind);
+}
+
+
+vec4	march(vec3 pos, vec3 dir)
+{
+  vec2	dist = vec2(0.0, 0.0);
+  vec3	p = vec3(0.0, 0.0, 0.0);
+  vec4	step = vec4(0.0, 0.0, 0.0, 0.0);
+  vec3	dirr;
+
+  for (int i = -1; i < I_MAX; ++i)
+  {
+    dirr = dir;
+    rotate(dirr.xy, .51025*dist.y+iTime*-.0 );
+    p = pos + dirr * dist.y;
+    dist.x = scene(p);
+    dist.y += dist.x*.5;
+    float	d = 5.;
+    h -= vec3(.3, .2, .3)*.1/ (d+.0);
+    h += (
+    .001/(dist.x*dist.x+0.01)
+    -
+    1./(dist.y*dist.y+40.)
+    )
+    *
+    vec3
+    (
+    1.+(sin(1.0*(id.y+id.x+id.z)+0.00) )
+    ,
+    1.+(sin(1.0*(id.x+id.y+id.z)+1.04) )
+    ,
+    1.+(sin(1.0*(id.z+id.x+id.z)+2.08) )
+    )*.5;
+    // log trick by aiekick
+    if (log(dist.y*dist.y/dist.x/1e5)>0. || dist.x < E || dist.y >= FAR)
+    {
+      if (dist.x < E || log(dist.y*dist.y/dist.x/1e5)>0.)
+      step.y = 1.;
+      break;
+    }
+    step.x++;
   }
+  step.w = dist.y;
+  return (step);
+}
 
-  vec3 bg= vec3(.2, .1, .2);
-  vec3 c=mix(vec3(.7, .1, .1), bg, uv.x*4.3+ri);
-  c.g+=sin(iTime);
-  c+=g*.02;
-  c=mix(c, bg,1.-exp(-.01*t*t));
-  fragColor = vec4(c,1.0);
+// Utilities
+
+float	mylength(vec3 p)
+{
+  float	ret = 1e5;
+
+  p = p*p;
+  p = p*p;
+  p = p*p;
+
+  ret = p.x + p.y + p.z;
+  ret = pow(ret, 1./8.);
+
+  return ret;
+}
+
+float	mylength(vec2 p)
+{
+  float	ret = 1e5;
+
+  p = p*p;
+  p = p*p;
+  p = p*p;
+
+  ret = p.x + p.y;
+  ret = pow(ret, 1./8.);
+
+  return ret;
+}
+
+void rotate(inout vec2 v, float angle)
+{
+  v = vec2(cos(angle)*v.x+sin(angle)*v.y,-sin(angle)*v.x+cos(angle)*v.y);
+}
+
+vec2	rot(vec2 p, vec2 ang)
+{
+  float	c = cos(ang.x);
+  float	s = sin(ang.y);
+  mat2	m = mat2(c, -s, s, c);
+
+  return (p * m);
+}
+
+
+vec3 calcNormal( in vec3 pos, float e, vec3 dir)
+{
+  vec3 eps = vec3(e,0.0,0.0);
+
+  return normalize(vec3(
+  march(pos+eps.xyy, dir).w - march(pos-eps.xyy, dir).w,
+  march(pos+eps.yxy, dir).w - march(pos-eps.yxy, dir).w,
+  march(pos+eps.yyx, dir).w - march(pos-eps.yyx, dir).w ));
+}
+
+vec3	camera(vec2 uv)
+{
+  float		fov = 1.;
+  vec3		forw  = vec3(0.0, 0.0, -1.0);
+  vec3    	right = vec3(1.0, 0.0, 0.0);
+  vec3    	up    = vec3(0.0, 1.0, 0.0);
+
+  return (normalize((uv.x) * right + (uv.y) * up + fov * forw));
 }
 
 void main() {
@@ -121,7 +224,7 @@ window.addEventListener("keydown", (event) => {
     let numberKey = +event.key;
     if (Number.isNaN(numberKey))
         return;
-    scaleFactor = numberKey === 0 ? 0.5 : numberKey;
+    scaleFactor = Math.pow(2, numberKey - 1);
     resize(gl);
 });
 resize(gl);
