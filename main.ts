5dc0c3ba0dcee05d8a1f21c49138bbaac95e105c
Maximilien Mellen <maxmellen0@gmail.com>
2020-05-12 22:06:48 +0200

    Draw Cobweb shader
    Original: https://www.shadertoy.com/view/3d2cRV

--- main.ts
+++ main.ts
@@ -5,15 +5,12 @@ if (!gl) {
   throw new Error("This browser does not support WebGL 2.");
 }
 
-gl.canvas.width = canvas.clientWidth;
-gl.canvas.height = canvas.clientHeight;
+let scaleFactor = 4;
 
-if (window.devicePixelRatio) {
-  gl.canvas.width *= window.devicePixelRatio;
-  gl.canvas.height *= window.devicePixelRatio;
-}
-
-let positions = Float32Array.of(...[0, 0], ...[1, 0], ...[0, 0.5]);
+let positions = Float32Array.of(
+  ...[...[-1, 1], ...[-1, -1], ...[1, 1]],
+  ...[...[1, 1], ...[-1, -1], ...[1, -1]]
+);
 
 // language=GLSL
 let vsSource = `#version 300 es
@@ -27,10 +24,24 @@ void main() {
 // language=GLSL
 let fsSource = `#version 300 es
 precision mediump float;
+uniform vec2 u_resolution;
+uniform float u_time;
 out vec4 out_color;
 
+void mainImage(out vec4 e) {
+  vec3 d = 0.5 - vec3(gl_FragCoord.xy, 1) / u_resolution.y, p, o;
+  for (int i = 0; i < 32; i++) {
+    o = p;
+    o.z -= u_time * 9.0;
+    float a = o.z * 0.1;
+    o.xy *= mat2(cos(a), sin(a), -sin(a), cos(a));
+    p += (0.1- length(cos(o.xy) + sin(o.yz))) *d;
+  }
+  e = vec4((sin(p) + vec3(2.0, 5.0, 9.0)) / length(p) * vec3(1.0), 1.0);
+}
+
 void main() {
-  out_color = vec4(1.0, 0.0, 0.5, 1.0);
+  mainImage(out_color);
 }
 `;
 
@@ -40,6 +51,8 @@ let program = linkProgram(gl, vs, fs);
 
 let positionBuffer = gl.createBuffer();
 let positionAttrib = gl.getAttribLocation(program, "a_position");
+let resolutionUniform = gl.getUniformLocation(program, "u_resolution");
+let timeUniform = gl.getUniformLocation(program, "u_time");
 
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
@@ -49,11 +62,37 @@ gl.vertexAttribPointer(positionAttrib, 2, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
 
-gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+window.addEventListener("resize", () => resize(gl!));
 
-gl.clearColor(0, 0, 0, 0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-gl.drawArrays(gl.TRIANGLES, 0, 3);
+window.requestAnimationFrame(function loop() {
+  draw(gl!);
+  window.requestAnimationFrame(loop);
+});
+
+window.addEventListener("keydown", (event) => {
+  let numberKey = +event.key;
+  if (Number.isNaN(numberKey)) return;
+  scaleFactor = numberKey === 0 ? 0.5 : numberKey;
+  resize(gl!);
+});
+
+resize(gl);
+
+let startTime = Date.now();
+
+function resize(gl: WebGL2RenderingContext) {
+  gl.canvas.width = canvas.clientWidth / scaleFactor;
+  gl.canvas.height = canvas.clientHeight / scaleFactor;
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.uniform2f(resolutionUniform, gl.canvas.width, gl.canvas.height);
+}
+
+function draw(gl: WebGL2RenderingContext) {
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.uniform1f(timeUniform, (Date.now() - startTime) / 1000);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
 
 function compileShader(
   gl: WebGL2RenderingContext,
